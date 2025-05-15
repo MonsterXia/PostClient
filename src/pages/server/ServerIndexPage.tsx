4d0f13f31bfe68b86029a2d859fb21a5ac80f6ce
MonsterXia <monster1018629681@gmail.com>
2025-05-15 09:40:11 +0800

    achieve register

--- src/pages/server/ServerIndexPage.tsx
+++ src/pages/server/ServerIndexPage.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import React from "react";
 const ServerIndex: React.FC = () => {
   return (
     <div>
