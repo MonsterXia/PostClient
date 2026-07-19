32db36a98c27b870a897007451cb1df6f661911f
MonsterXia <monster1018629681@gmail.com>
2026-07-19 13:19:02 +0800

    Revert "restore: add back questionares pages and related files from release branch"
    This reverts commit 60059fdec1b8b781be45aaa4cb583f29187431c6.

--- src/utils/push.tsx
+++
@@ -1,6 +0,0 @@
-import { submitLayerImprovmentFormAPI } from "@/apis";
-
-export const pushLayerImprovement = async (data: unknown) => {
-  const res = await submitLayerImprovmentFormAPI(data);
-  return res;
-};
